06f9514bfea0be4b2824d52e792d3cceb3ad6fdc
DESKTOP-GURND3D\TIEN <xuantien24689@gmail.com>
2026-06-13 11:58:36 +0700

    conversation member

---
+++ RealtimeChat.Application/DTOs/ConversationMembers/UpdateConversationMemberRoleRequest.cs
@@ -0,0 +1,11 @@
+﻿using RealtimeChat.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealtimeChat.Application.DTOs.ConversationMembers;
+
+public class UpdateConversationMemberRoleRequest
+{
+    public ConversationMemberRole Role { get; set; }
+}
---
+++ RealtimeChat.Application/Service/Interfaces/IConversationMemberService.cs
@@ -0,0 +1,20 @@
+﻿using RealtimeChat.Application.DTOs.ConversationMembers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealtimeChat.Application.Service.Interfaces
+{
+    public interface IConversationMemberService
+    {
+        Task<bool> AddMemberAsync(string conversationId, AddConversationMemberRequest request);
+
+        Task<bool> RemoveMemberAsync(string conversationId, string userId);
+
+        Task<bool> UpdateRoleAsync(string conversationId, string userId, UpdateConversationMemberRoleRequest request);
+
+        Task<int> CountUnreadMessagesAsync(string conversationId, string userId);
+
+        Task<bool> MarkAsReadAsync(string conversationId, string userId, MarkConversationReadRequest request);
+    }
+}
