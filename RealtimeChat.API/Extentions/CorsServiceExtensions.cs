6f0e7addf0cf4b8b9481c6666c44ca9e8d566bda
DESKTOP-GURND3D\TIEN <xuantien24689@gmail.com>
2026-09-23 14:44:15 +0700

    setup deloy

--- RealtimeChat.API/Extentions/CorsServiceExtensions.cs
+++ RealtimeChat.API/Extentions/CorsServiceExtensions.cs
@@ -4,13 +4,17 @@ public static class CorsServiceExtensions
 {
     public const string AngularAppPolicy = "AllowAngularApp";
 
-    public static IServiceCollection AddCorsPolicy(this IServiceCollection services)
+    public static IServiceCollection AddCorsPolicy(this IServiceCollection services, IConfiguration configuration)
     {
+        var allowedOrigins = configuration.GetSection("Cors:AllowedOrigins").Get<string[]>();
+        if (allowedOrigins == null || allowedOrigins.Length == 0)
+            allowedOrigins = ["http://localhost:4200"];
+
         services.AddCors(options =>
         {
             options.AddPolicy(AngularAppPolicy, policy =>
             {
-                policy.WithOrigins("http://localhost:4200")
+                policy.WithOrigins(allowedOrigins)
                       .AllowAnyMethod()
                       .AllowAnyHeader()
                       .AllowCredentials();
